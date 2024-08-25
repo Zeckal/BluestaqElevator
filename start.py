4a30a2854a4790660a68e73754476d248e4408be
Zeckal <Zeckal4@gmail.com>
2024-08-24 19:05:16 -0500

    documentation

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-
+# By Andrew Shaw 8/24/2024
 
 def main():
    
@@ -29,6 +29,7 @@ def main():
        cycleInput = input() 
 
 class Building(object):
+    #object to hold the data model that it interacted with
     elevators = []
     callButtons = []
     def __init__ (self,floors,elevators):
@@ -38,16 +39,20 @@ class Building(object):
             self.AddCallButton(y)
         
     def AddElevator(self,floors,name):
+        #constructor assitant function
         self.elevators.append(Elevator(floors,name))
         
     def AddCallButton(self,floor):
+        #constructor assitant function
         self.callButtons.append(CallButton(floor))
         
     def Cycle(self):
+        #step to the next moment in time, make all the elevators take their next action
         for e in self.elevators:
             e.Cycle(self.callButtons,self.elevators)
             
     def Print(self):
+        #print out all internal objects for console display
         for elv in self.elevators:
             elv.print()
         for callbutton in self.callButtons:
@@ -129,95 +134,98 @@ class Elevator(object):
             self.currentFloor += direction
             
     def Cycle(self, callButtons, otherElevators):
-        #cycle function does one action per time it is called, this could be changed to a countdown if timespands needed to be non-uniform
+        #make the elevator take it's next action
+        #elevators need to check 4 main things to determine what it is doing
+        # 1: what direction it is currently traveling
+        # 2: if any of it's buttons have been pressed
+        # 3: if any call buttons have been pressed
+        # 4: if any other elevator is already traveling to a location
+        # 5: less important - is if it needs to close it's own doors
         
-        
+        #make a list of all currently targeted floors
         otherTargets = []
         for elv in otherElevators:
             otherTargets.append(elv.targetFloor)
             
-        
+        #close doors if they are open, this will be the action the elevator takes this cycle
         if self.doorsOpen == 1:
-            #if the doors are open, close the doors
             self.doorsOpen = 0
-            
+        
+        #if it is moving upwards
         elif self.movement == 1:
+                #check if the upwards call button or it's own panel button has been pressed for this floor
+                #if so, open the doors and turn off the buttons
             if callButtons[self.currentFloor-1].upButtonPressed == 1 or self.pannelButtons[self.currentFloor-1].pressed == 1:
-                #if the doors are closed, check if they need to be opened, it is traveling upwards, so do not open for callbutton downards
                 self.doorsOpen = 1
-                #garunteed doors are closed due to first if block in cycle function
-                
                 callButtons[self.currentFloor-1].upButtonPressed = 0
                 self.pannelButtons[self.currentFloor-1].pressed = 0
-                
-                #turn the call and pannel buttons off as it has opened for this floor now
-               
+            
+            #check if it didn't just open it's doors and if it needs to continue upwards to another floor
             if self.doorsOpen == 0 and self.currentFloor != self.floorMax:
-                #it didn't need to open doors, check if it needs to continue upwards
                 for uup in range (self.currentFloor,self.floorMax):
                     #only check floors above current floor
                     if uup not in otherTargets and (self.pannelButtons[uup-1].pressed == 1 or callButtons[uup-1].upButtonPressed == 1 or callButtons[uup-1].downButtonPressed == 1):
                         #if any floor above has been pressed, keep traveling upwards
                         self.targetFloor = uup
-            
+            #check if it's doors didn't just open and if it needs to descend to another floor
             if self.doorsOpen == 0 and self.currentFloor == self.targetFloor:
-                #if it doesn't need to continue upwards, check if it needs to go downwards
                 for udwn in range (self.floorMin-1,self.currentFloor):
                     #only check floors above current floor
                     if(self.pannelButtons[udwn-1].pressed == 1 or callButtons[udwn-1].upButtonPressed == 1 or callButtons[udwn-1].downButtonPressed == 1):
                         #if any floor below has been pressed, change to traveling downwards
                         self.targetFloor = udwn
                         self.movement = -1
+            #if no new destination is found, stop moving
             if self.doorsOpen == 0 and self.currentFloor == self.targetFloor:
-                #if no new target floor was found, stop
                 self.movement = 0
+            #if it didn't need to stop at this floor, continue upwards
             if self.movement == 1 and self.doorsOpen == 0:
                 self.currentFloor += 1
-            
+         
+        #if it is moving downwards   
         elif self.movement == -1:
+            #check if the downwards call button or it's own panel button has been pressed for this floor
+            #if so, open the doors and turn off the buttons
             if callButtons[self.currentFloor-1].downButtonPressed == 1 or self.pannelButtons[self.currentFloor-1].pressed == 1:
-                #if the doors are closed, check if they need to be opened, it is traveling downwards, so do not open for callbutton upwards
-                
                 self.doorsOpen = 1
-                #garunteed doors are closed due to first if block in cycle function
-                
                 callButtons[self.currentFloor-1].downButtonPressed = 0
                 self.pannelButtons[self.currentFloor-1].pressed = 0
-                #turn the call and pannel buttons off as it has opened for this floor now
                 
+            #check if it didn't just open it's doors and if it needs to continue downwards to another floor
             if self.doorsOpen == 0 and self.currentFloor != self.floorMin:
-                #it didn't need to open doors, check if it needs to continue downwards
                 for ddwn in range (self.floorMin-1,self.currentFloor):
                     #only check floors below current floor
                     if ddwn not in otherTargets and (self.pannelButtons[ddwn-1].pressed == 1 or callButtons[ddwn-1].upButtonPressed == 1 or callButtons[ddwn-1].downButtonPressed == 1):
                         #if any floor below has been pressed, keep traveling downwards
                         self.targetFloor = ddwn 
+            #check if it's doors didn't just open and if it needs to ascend to another floor
             if self.currentFloor == self.targetFloor:
-                #if it doesn't need to continue upwards, check if it needs to go downwards
                 for dup in range (self.currentFloor+1,self.floorMax):
                     #only check floors above current floor
                     if(self.pannelButtons[dup-1].pressed == 1 or callButtons[dup-1].upButtonPressed == 1 or callButtons[dup-1].downButtonPressed == 1):
-                        #if any floor below has been pressed, change to traveling downwards
+                        #if any floor above has been pressed, change to traveling upwards
                         self.targetFloor = dup
                         self.movement = 1
+                        
+            #if no new destination is found, stop moving
             if self.currentFloor == self.targetFloor:
-                #if no new target floor was found, stop
                 self.movement = 0
+            #if it didn't need to stop at this floor, continue downwards
             if self.movement == -1 and self.doorsOpen == 0:
                 self.currentFloor += -1
-                
+        
+        #if it is not moving
         elif self.movement == 0:
+            #check to see if the buttons on it's current floor have been pressed
+            #if so, open the doors and turn off the buttons
+            #this time both upwards and downwards buttons get reset since the elevator didn't have any initial direction
             if self.pannelButtons[self.currentFloor-1].pressed == 1 or callButtons[self.currentFloor-1].downButtonPressed == 1 or callButtons[self.currentFloor-1].upButtonPressed == 1:
                 self.doorsOpen == 1
-                #garunteed doors are closed due to first if block in cycle function
-                
-                
-                
                 callButtons[self.currentFloor-1].downButtonPressed = 0
                 callButtons[self.currentFloor-1].upButtonPressed = 0
                 self.pannelButtons[self.currentFloor-1].pressed = 0
-                #turn the call and pannel buttons off as it has opened for this floor now
                 
+            #next check if any buttons have been pressed that are not already targeted by another elevator
             distance = 0
             for floor in range(self.floorMin,self.floorMax):
                 # find the closest floor that has been called
@@ -226,9 +234,10 @@ class Elevator(object):
                         distance = abs(self.currentFloor - floor)
                         self.targetFloor = floor
                 
-                        
+            #if the target is above, start moving upwards            
             if self.targetFloor > self.currentFloor:
                 self.movement = 1
+            #if the target is below, start moving downwards   
             elif self.targetFloor < self.currentFloor:
                 self.movement = -1
             else:
